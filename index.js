3716595e3a8e4117e6b41af6ca229df1af948bda
Sean L <leonardmsean@gmail.com>
2023-04-26 21:19:12 -0400

    finished promise with .then and wrote logo file writing function...

--- index.js
+++ index.js
@@ -17,32 +17,50 @@ const colors = [
   "violet",
 ];
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "text",
-    message: "Enter text (up to 3 characters):",
-  },
-  {
-    type: "list",
-    name: "textColor",
-    message: "Choose text color: ",
-    choices: colors,
-  },
-  {
-    type: "list",
-    name: "shapeIndex",
-    message: "Choose a shape:",
-    choices: [
-      { name: "Circle", value: 0 },
-      { name: "Triangle", value: 1 },
-      { name: "Square", value: 2 },
-    ],
-  },
-  {
-    type: "list",
-    name: "shapeColor",
-    message: "Choose a shape color: ",
-    choices: colors,
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter text (up to 3 characters):",
+    },
+    {
+      type: "list",
+      name: "textColor",
+      message: "Choose text color: ",
+      choices: colors,
+    },
+    {
+      type: "list",
+      name: "shapeIndex",
+      message: "Choose a shape:",
+      choices: [
+        { name: "Circle", value: 0 },
+        { name: "Triangle", value: 1 },
+        { name: "Square", value: 2 },
+      ],
+    },
+    {
+      type: "list",
+      name: "shapeColor",
+      message: "Choose a shape color: ",
+      choices: colors,
+    },
+  ])
+
+  .then((answers) => {
+    const { text, textColor, shapeIndex, shapeColor } = answers;
+
+    const shapeClass = shape[shapeIndex];
+    const shape = new shapeClass(shapeColor);
+
+    const svg = `
+    <svg width="300" height="200">
+      ${shape.render()}
+      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}">${text}</text>
+    </svg>
+  `;
+
+    fs.writeFileSync("logo.svg", svg);
+    console.log("Generated logo.svg");
+  });
